f0cd519de7f5023fd0418a26fa902256b6f1c205
sagar25444x <sagar25444x@gmail.com>
2026-05-27 01:41:09 +0530

    solve

--- DAY-2 LEC-3/HW/HW1.cpp
+++ DAY-2 LEC-3/HW/HW1.cpp
@@ -1,13 +1,28 @@
 // Problem 1: Write a program to find the sum of all numbers from 1 to N that are divisible by 3.
 
-
 #include <iostream>
 using namespace std;
+int sumDivisibleBy3(int N)
+{
+  int sum = 0;
 
-int main() {
+  for (int i = 1; i <= N; i++)
+  {
+    if (i % 3 == 0)
+    {
+      sum = sum + i;
+    }
+  }
 
-  
-    
+  return sum;
+}
+int main()
+{
+  int N;
+  cout<<"range : ";
+  cin >> N;
 
-    return 0;
+  cout << sumDivisibleBy3(N);
+
+  return 0;
 }
