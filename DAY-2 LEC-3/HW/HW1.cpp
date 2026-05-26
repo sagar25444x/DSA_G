89bce2e60b02101f0aacb88895590ba133922ff7
sagar25444x <sagar25444x@gmail.com>
2026-05-26 12:48:57 +0530

    notess

---
+++ DAY-2 LEC-3/HW/HW1.cpp
@@ -0,0 +1,13 @@
+// Problem 1: Write a program to find the sum of all numbers from 1 to N that are divisible by 3.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  
+    
+
+    return 0;
+}
