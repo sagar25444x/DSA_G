75bbcb198f5844f5efe59e1907462fa1c98d0fe0
sagar25444x <sagar25444x@gmail.com>
2026-05-29 16:09:08 +0530

    nn

--- DAY-3/DAY 2/tempCodeRunnerFile.cpp
+++ DAY-3/DAY 2/tempCodeRunnerFile.cpp
@@ -1,2 +1,40 @@
+#include <iostream>
+using namespace std;
 
-    // spaces
+int main() {
+    int n = 5;
+
+    // Upper part
+    for (int i = 0; i < n; i++) {
+
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower part
+    for (int i = n - 2; i >= 0; i--) {
+
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
