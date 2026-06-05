bb9487e2601d153efebece727143648862e28920
sagar25444x <sagar25444x@gmail.com>
2026-06-06 00:17:03 +0530

    n

--- DAY-3/STRIVER_PATTERN/pattern11.cpp
+++ DAY-3/STRIVER_PATTERN/pattern11.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 5;
+int main()
+{
+  int n = 5;
 
-    for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
 
-        // Decide starting number
-        int start;
-        if (i % 2 == 0) {
-            start = 1;
-        }
-        else {
-            start = 0;
-        }
-
-        // Print pattern
-        for (int j = 0; j <= i; j++) {
-            cout << start << " ";
-
-            // Toggle between 0 and 1
-            if (start == 1) {
-                start = 0;
-            }
-            else {
-                start = 1;
-            }
-        }
-
-        cout << endl;
+    // Decide starting number
+    int start;
+    if (i % 2 == 0)
+    {
+      start = 1;
+    }
+    else
+    {
+      start = 0;
     }
 
-    return 0;
+    // Print pattern
+    for (int j = 0; j <= i; j++)
+    {
+      cout << start << " ";
+
+      // Toggle between 0 and 1
+      if (start == 1)
+      {
+        start = 0;
+      }
+      else
+      {
+        start = 1;
+      }
+    }
+
+    cout << endl;
+  }
+
+  return 0;
 }
